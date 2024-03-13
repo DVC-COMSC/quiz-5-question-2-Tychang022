858d798cba63028c423e639086c1709bf784edd4
Tychang022 <156855881+Tychang022@users.noreply.github.com>
2024-03-13 03:28:18 +0000

    TC

--- main.cpp
+++ main.cpp
@@ -5,8 +5,30 @@ int main()
 {
    int N, M;
    
-   cout << "Enter your N, M(0 < N < 10,  1 <= M <= 10): ";
-   cin >> N >> M;
-   /* Complete your code */
+   do{
+      cout << "Enter your N, M(0 < N < 10,  1 <= M <= 10): ";
+      cin >> N >> M;
+      if ( !(0 < N < 10) || !(1 <= M <= 10))
+      cout << "Error, Please try again\n";
+   }while (!(0 < N < 10) || !(1 <= M <= 10));
+   
+   int nthPower = 1;
+	
+	
+	if (N >= 0){
+		for(int i = 0; i < N; ++i)
+		{
+		nthPower *= M;
+		}
+	}
+	else{
+		for(int i=0; i > N; --i){
+		nthPower /= M;
+		}
+	}
+								
+	
+	cout << "M to Power " << N << " is " << nthPower << endl;
+
   
 }
